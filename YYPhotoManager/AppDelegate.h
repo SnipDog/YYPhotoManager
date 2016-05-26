ee424a78bb0939f64c993eb9181b5c205f6bfb49
hedongbin <5690316@qq.com>
2016-05-27 02:29:50 +0800

    Initial Commit

---
+++ YYPhotoManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YYPhotoManager
+//
+//  Created by Heisenbean on 16/5/27.
+//  Copyright © 2016年 Heisenbean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YYPhotoManager/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YYPhotoManager
+//
+//  Created by Heisenbean on 16/5/27.
+//  Copyright © 2016年 Heisenbean. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
