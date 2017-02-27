534d3e9a0f3487e12df73b246d3f8131cf59ffb1
Heisenbean <hedongbin1991@gmail.com>
2017-02-27 18:44:15 +0800

    change respo

--- YYPhotoManager.podspec
+++ YYPhotoManager.podspec
@@ -2,15 +2,15 @@ Pod::Spec.new do |s|
 s.name             = "YYPhotoManager"
 s.version          = "1.0"
 s.summary          = "Simple manager to take photo or select photo from album."
-s.homepage         = "https://github.com/Heisenbean/YYPhotoManager/tree/master/Resource"
+s.homepage         = "https://github.com/Heisenbean/YYPhotoManager"
 s.license          = "MIT"
 s.author           = { "Heisenbean" => "heisenbean.me@gmail.com" }
-s.source           = { :git => "https://github.com/Heisenbean/YYPhotoManager", :tag => "1.0" }
+s.source           = { :git => "https://github.com/Heisenbean/YYPhotoManager.git", :tag => "1.0" }
 s.social_media_url = "https://twitter.com/hedongbin"
-s.source_files     = "YYPhotoManager/Resource/*.{h,m}"
+s.source_files     = "YYPhotoManager/*.{h,m}"
 s.exclude_files    = "YYPhotoManager/*.{h,m}"
-s.resource_bundles = { "YYPhotoManager/Resource" => "YYPhotoManager/Resource/*.{lproj,storyboard,xib,nib}" }
+s.resource_bundles = { "YYPhotoManager" => "YYPhotoManager/*.{lproj,storyboard,xib,nib}" }
 s.platform         = :ios, "8.0"
 s.requires_arc     = true
-s.frameworks       = "Photos"
+# s.frameworks       = "Photos"
 end
---
+++ YYPhotoManagerDemo/ViewController.m
@@ -0,0 +1,66 @@
+//
+//  ViewController.m
+//  YYPhotoManager
+//
+//  Created by Heisenbean on 16/5/27.
+//  Copyright © 2016年 Heisenbean. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "YYPhotoManager.h"
+@interface ViewController () <YYPhotoManagerDelegate>
+@property (strong,nonatomic) YYPhotoManager *manager;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+- (IBAction)singleSelection:(id)sender {
+    self.manager.isCustom = NO;
+    self.manager.isAllowMultiSelect = NO;
+    [self.manager show];
+}
+
+- (IBAction)multiSelection:(id)sender {
+    self.manager.isCustom = NO;
+    self.manager.isAllowMultiSelect = YES;
+    self.manager.maxSelections = 6;
+    [self.manager show];
+}
+
+- (IBAction)showCustomSheet:(id)sender {
+    self.manager.isCustom = YES;
+    [self.manager show];
+}
+
+
+- (void)didFinishEditPhoto:(UIImage *)image{
+    self.imageView.image = image;
+}
+
+- (void)didFinishTakePhoto:(UIImage *)image{
+    self.imageView.image = image;
+}
+
+- (void)didFinishPickingPhotos:(NSArray *)images{
+    NSLog(@"%@",images);
+}
+
+
+#pragma mark - lazy
+- (YYPhotoManager *)manager{
+    if (!_manager) {
+        _manager = [YYPhotoManager defaultManager];
+        _manager.delegate = self;
+        _manager.target = self;
+    }
+    return _manager;
+}
+
+@end
